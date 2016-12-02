68327b7376bbb5dde0f7a0fff2d60e9f6a8f8690
Justin Tallant <jtallant07@gmail.com>
2016-12-02 14:31:41 -0600

    fix relationship for following

--- models.rb
+++ models.rb
@@ -1,13 +1,26 @@
 class User < ActiveRecord::Base
-	  has_many :posts
-	  has_many :follows
+  has_many :posts
+
+  has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_id'
+  has_many :active_relationships, class_name: 'Follow', foreign_key: 'user_id'
+
+  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :following, through: :active_relationships,  source: :target
+
+  def follow(target)
+    Follow.create(user_id: id, target_id: target.id)
+  end
+
+  def unfollow(target)
+    Follow.where(user_id: id, target_id: target.id).destroy
+  end
 end
 
 class Follow < ActiveRecord::Base
-	belongs_to :user_id, :class_name => 'User', :foreign_key => 'user_id'
-	belongs_to :target_id, :class_name => 'User', :foreign_key => 'target_id'
+  belongs_to :follower, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :target, class_name: 'User', foreign_key: 'target_id'
 end
 
 class Post < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
 end
