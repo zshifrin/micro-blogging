f857114425dad5d3416ea0484cd07d05e8194f5f
zshifrin <zach.shifrin@gmail.com>
2016-12-05 14:58:40 -0600

    added edit profile button

--- public/mockups/profile.php
+++ public/mockups/profile.php
@@ -3,16 +3,17 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-4">
-			<img src="http://placehold.it/250x250" class="img-responsive" alt="responsive profile image">
+			<img src="http://placehold.it/250x250" class="img-responsive img-rounded" alt="responsive profile image">
 			<h2>Username</h2>
 			<button type="button" class="btn btn-sm btn-primary">Follow</button>
 			<p id="profileBio">Bio: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate perspiciatis nam facilis aperiam fuga itaque incidunt delectus, reprehenderit veniam ratione minima, possimus voluptas aliquid sed temporibus, excepturi, modi voluptatibus. Adipisci.</p>
+			<button type="button" id="editProfileBtn" class="btn btn-sm btn-info">Edit Profile</button>
 		</div>
 
 		<div class="col-md-8">
 		<input type="text" class="form-control" placeholder="Post Title">
 		<textarea class="form-control" id="postBodyForm" rows="6"></textarea>
-		<button type="submit" class="btn btn-default">Submit</button>
+		<button type="submit" class="btn btn-primary">Submit</button>
 		</div>
 	</div>
 	<div class="row">
