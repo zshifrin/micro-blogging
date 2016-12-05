38c3735a767f2cf063027efde3dafc234670a3d5
Justin Tallant <jtallant07@gmail.com>
2016-12-05 11:55:11 -0600

    use partials for mockups

--- public/mockups/_header.php
+++ public/mockups/_header.php
@@ -30,33 +30,3 @@
 
     <div id="main">
         <div class="container">
-            <%= yield %>
-        </div>
-    </div>
-
-
-    <div id="site-footer">
-        <div class="container">
-            <div class="col-sm-6">
-                <p>A personal messaging service for goat lovers.</p>
-                <p>
-                    Pygmy was developed by: <b>The first NYCDA Austin</b>
-                </p>
-            </div>
-
-            <div class="col-sm-6">
-                <div id="social" class="pull-right">
-                    <a href="http://www.facebook.com"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
-                    <a href="http://www.twitter.com"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
-                </div>
-            </div>
-        </div>
-    </div> <!-- #site-footer -->
-    <div id="bottomstrip"> We appreciate your visit to Pygmy - Come back and see us!</div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
-
-</html>
---
+++ public/mockups/sign-up.php
@@ -0,0 +1,4 @@
+<?php require '_header.php'; ?>
+<h1>Hello World</h1>
+<p>This is the unique part of this page</p>
+<?php require '_footer.php'; ?>
